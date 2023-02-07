07a22bb1970252c32c61de1ef05dfa79d8c633ab
Rizki Budi Prasetyo <rizkibudi513@gmail.com>
2023-02-07 17:24:19 +0700

    config: setup next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const BASE_API_URL = process.env.ENABLE_MOCK_API
+  ? "https://localhost:3000/api"
+  : process.env.BASE_API_URL || "https://localhost:3000/api";
+
+const nextConfig = {
+  reactStrictMode: process.env.NODE_ENV === "production",
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+  rewrites: async () => [
+    {
+      source: "/api/:path*",
+      destination: BASE_API_URL + "/:path*",
+    },
+  ],
+};
+
+module.exports = nextConfig;
