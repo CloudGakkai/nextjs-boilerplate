c913e0323cecd8110abbc8bb58d88d9fcbfd6df3
Rizki Budi Prasetyo <rizkibudi513@gmail.com>
2023-02-07 17:59:21 +0700

    config: install tailwindcss plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,9 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/typography"),
+  ],
 };
